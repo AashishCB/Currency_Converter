b8cb68519a786ac0b90d83a3043ff30594daa846
Aashish Chandra Bivek <aashishcb@Aashishs-MacBook-Pro.local>
2022-08-26 20:48:25 +0545

    predefine functions create requirements file create virtual environment

---
+++ checks.py
@@ -0,0 +1,41 @@
+import datetime
+import sys
+
+def check_arguments(args):
+    """
+    Function that will check if there are enough input arguments provided (ie exactly 3) and will return the input arguments if it is the case.
+    Otherwise the program will exit and display the relevant message provided in the assignment brief
+
+    Parameters
+    ----------
+    # => To be filled
+
+    Pseudo-code
+    ----------
+    # => To be filled
+
+    Returns
+    -------
+    # => To be filled
+    """
+
+    # => To be filled
+
+def check_date(date):
+    """
+    Function that will check if the provided date is valid and will return the value True if that is the case.
+    Otherwise the program will exit and display the relevant message provided in the assignment brief
+
+    Parameters
+    ----------
+    # => To be filled
+
+    Pseudo-code
+    ----------
+    # => To be filled
+
+    Returns
+    -------
+    # => To be filled
+    """
+    # => To be filled
